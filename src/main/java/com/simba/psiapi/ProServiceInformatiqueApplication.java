3990a7edc09e8f39f269844a1a70a70cfbcda88b
ElHadji Omar DIONE <omardione1991@gmail.com>
2021-07-01 16:04:08 +0100

    initial commit

---
+++ src/main/java/com/simba/psiapi/ProServiceInformatiqueApplication.java
@@ -0,0 +1,13 @@
+package com.simba.psiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProServiceInformatiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProServiceInformatiqueApplication.class, args);
+	}
+
+}
